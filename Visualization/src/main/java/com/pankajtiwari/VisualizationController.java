01b08f51db588621827cc00835303d36500134a4
impankajtiwari <mail.tiwaripankaj@gmail.com>
2015-10-10 21:45:56 -0700

    First Maven Project Commit

---
+++ Visualization/src/main/java/com/pankajtiwari/VisualizationController.java
@@ -0,0 +1,27 @@
+package com.pankajtiwari;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author pankajtiwari
+ */
+@Controller
+@RequestMapping("/launch")
+public class VisualizationController {
+
+	String message = "Welcome to Spring MVC!";
+	 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
